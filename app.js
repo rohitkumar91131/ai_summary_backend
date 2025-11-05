795a43b74d8d13fa03068085fb82694df838ce5d
Rohit <rk341190100@gmail.com>
2025-11-05 12:30:58 +0530

    added frontend url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import './config/db.js';
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials : true,
 }));
 app.use(express.json());
